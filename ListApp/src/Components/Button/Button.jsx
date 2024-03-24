42ce2340cb4e69b317ee12fac97a745a02774bdc
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2024-03-23 22:47:57 -0500

    Add files via upload

---
+++ ListApp/src/Components/Button/Button.jsx
@@ -0,0 +1,12 @@
+export const Button = (props) => {
+
+    const {title, type, onClick, uid} = props
+
+    const handleClick = () => onClick(type)
+
+    return (
+    <button onClick={handleClick} className="button" id = {uid}>
+    {title}
+
+    </button>)
+  };
